25131d3e4c0e02a917ef6f6102ecd39b12758154
wt-854 <weiwentan1993@gmail.com>
2020-05-12 19:10:08 +0800

    finished ep5

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+
+  dateStampDp: any;
+}
