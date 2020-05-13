a5c8e7f53554f398cd483142f238abb75cc9c6ab
weiwentan1993 <weiwentan1993@gmail.com>
2020-05-13 17:35:55 +0800

    finished topic 12 - submitting form data

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import * as moment from 'moment';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,21 @@ export class AppComponent {
 
   userModel = new User('Rob', 'rob@test.com', 5556665656, 'default', 'morning', true);
 
+  constructor(private _enrollmentService: EnrollmentService) {}
+
   validateTopic(value) {
     if (value === 'default') {
       this.topicHasError = true;
     } else {
       this.topicHasError = false;
     }
+  }
+
+  onSubmit() {
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        data => console.log('Success!', data),
+        error => console.log('Error!', error)
+      )
   }
 }
